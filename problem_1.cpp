449d6f00a2c7224c9765c2805ce3b5c09e951520
mirmamadiyev-rgb <mir.mamadiyev@newuu.uz>
2025-09-16 13:12:02 +0500

    Add files via upload

---
+++ problem_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//problem18
+int main() {
+    double a;
+    double b;
+    cout << "Please enter 2 numbers: ";
+    cin>>a;
+    cin>>b;
+    cout<<(a<140 && a<b)<<endl;
+    return 0;
+}

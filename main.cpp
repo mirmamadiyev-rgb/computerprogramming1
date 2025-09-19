40b8a9c94be62eb53d931c1f4e7779e970edd481
mirmamadiyev-rgb <mir.mamadiyev@newuu.uz>
2025-09-19 12:42:15 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,137 +1,121 @@
-/*  #include <iostream>
+#include <cmath>
+#include <iostream>
+#include <math.h>
 using namespace std;
-//problem1
-int main() {
-int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    if(a < 0) {cout << a << "is a negative number"<< endl; }
-    else { cout << a << " is a positive number"<< endl; }
-    return 0;
-
-} */
-
-//problem2
+//problem12
 /*
-#include <iostream>
-using namespace std;
 int main() {
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    cout << a << " is" ;
-    if(a < 0) {cout  << " negative"; }
-    else { cout << " positive"; }
-if (a % 2 == 0) {cout << " and odd number"<< endl; }
+    double a, b, c;
+cout << "Please enter the 3 angles of triangle: ";
+    cin >> a >> b >> c;
+    if (a + b + c == 180) {cout << "The triangle is valid "<< endl;}
+    else {cout << "The triangle is not valid "<< endl;}
+return 0;
 
-    else { cout  << " and even number"<< endl; }
+}     */
 
+//problem13
+/*
+int main() {
+    double a, b, c, delta,x1,x2;
+    cout << "Please enter values of a , b , c of the quadratic formula: ";
+    cin >> a >> b >> c;
+    delta = b * b - 4 * a * c;
+    x1 = (-b + sqrt(delta)) / (2 * a);
+    x2 = (-b - sqrt(delta)) / (2 * a);
+    if (delta < 0) {cout <<"there is no rational root of this equation"<<endl;}
+    else {cout <<"the roots of the equation are:"<<x1<<" and "<<x2<<endl;}
     return 0;
-} */
+}    */
 
 
-//problem3
-/*#include <iostream>
-using namespace std;
+//problem14
+/*
 int main() {
-    int a, b, c;
-cout << "Please enter 3 numbers : " << endl;
-    cin >>a>>b>>c;
-    int largest;
-    if (a >= b && a >= c) {cout << a << " is the largest " ;}
-    else if (b >= a && b >= c) {cout << b << " is the largest " ;}
-    else   {cout << c << " is the largest " ;}
-} */
+    char ch;
+    cout << "Enter a letter: ";
+    cin >> ch;
 
-//problem4
-/* #include <iostream>
-using namespace std;
-int main() {
-cout << "Enter two integer numbers : " << endl;
-    int a,b;
-    cin >>a>>b;
-    if (a % b == 0){cout << a<<" is divisible by " <<b<<endl;}
-    else cout << a<<" is not divisible by " <<b<<endl;
-    return 0;
-} */
-
-
-//problem5
- /* #include <iostream>
-using namespace std;
-int main() {
-    int a;
-    cout << "input a year : " << endl;
-cin >>a;
-    if (a%4==0 && a%100!=0 && a%400==0)
-    {cout <<a<<" is a leap year";}
-    else {cout <<a<<" is not a leap year";}
-    return 0;
-} */
-
-
-//problem6
-/* #include <iostream>
-using namespace std;
-int main() {
-    int a;
-    cout << "Enter a speed of the vehicle: ";
-    cin >> a;
-    if (a<20){cout << "too slow" << endl;}
-    else if (a>60){cout << "too fast" << endl;}
-    else {cout <<"just right"<<endl;}
-
-    return 0;
-} */
-
-
-
-//problem7
-/*  #include <iostream>
-using namespace std;
-int main() {
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    if (a>=50) {
-        cout <<"Pass"<<endl;
+    if (ch >= 'A' && ch <= 'Z') {
+        cout << "Uppercase alphabet" << endl;
     }
-        else {cout <<"Fail"<<endl;}
-    return 0;
-
-     }    */
-
-
-//problem8
- /* #include <iostream>
-using namespace std;
-int main() {
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    if (a<0){cout <<"negative number";}
-    else if (a>0){cout <<"positive number";}
-    else {cout <<"zero";}
-    return 0;
-} */
-
-
-//problem10
-#include <iostream>
-#include <conio.h>
-
-using namespace std;
-
-int main() {
-    cout << "Enter traffic light color: ";
-    char a= _getch();
-    switch (toupper(a)) {
-        case 'G': cout << "Go!" << endl; break;
-        case 'Y': cout << "Get ready!" << endl; break;
-        case 'R': cout << "Stop!" << endl; break;
-        default: cout << "Invalid input!" << endl;
+    else if (ch >= 'a' && ch <= 'z') {
+        cout << "Lowercase alphabet" << endl;
+    }
+    else {
+        cout << "Not an alphabet" << endl;
     }
 
     return 0;
+}          */
+
+
+//problem15
+/*
+int main() {
+    double a,b,c,d,p1,p2;
+    cout << "first enter the weight and price of the first package,and then second one's: ";
+    cin>>a>>b>>c>>d;
+    p1=a*b;
+    p2=c*d;
+    if (p1<0 || p2<0) {cout <<"vaues can not be negative!"<<endl;}
+else if (p1>p2){cout <<"First package is cheaper than the second one"<<endl;}
+    else {cout<<"The second package is cheaper than the first one "<<endl;}
+    return 0;
+}    */
+
+//problem16
+/*
+int main() {
+    int num;
+    cout << "Enter a three-digit integer: ";
+    cin >> num;
+
+    // Ensure positive for digit extraction
+    int absNum = (num < 0) ? -num : num;
+
+    if (absNum < 100 || absNum > 999) {
+        cout << "Not a three-digit number!" << endl;
+        return 0;
+    }
+
+    int firstDigit = absNum / 100;
+    int lastDigit = absNum % 10;
+
+    if (firstDigit == lastDigit) {
+        cout << num << " is a palindrome" << endl;
+    } else {
+        cout << num << " is not a palindrome" << endl;
+    }
+return 0;
+}  */
+
+
+//problem17
+/*
+int main() {
+    cout << "Please enter the letter (u for Uzbek, e for English , r for Russian , g for German) :" << endl;
+    char ch = getchar();
+switch (ch) {
+    case 'u':cout<<"Salom"<<endl; break;
+        case 'e':cout<<"hello"<<endl; break;
+        case 'r':cout<<"Privet"<<endl; break;
+        case 'g':cout<<"Hallo"<<endl; break;
+default:cout<<"I do not know this language"<<endl; break;
 }
+    return 0;
+}       */
+
+//problem18
+/*
+int main() {
+    double a,b,d;
+    cout << "Enter x and y :";
+    cin >> a >> b;
+    d=sqrt((a*a)+(b*b));
+    if (d<10){cout<<"This point lies inside the circle "<<endl;}
+    else {cout<<"This point does not lie inside the circle"<<endl;}
+return 0;
+}        */
+
 
